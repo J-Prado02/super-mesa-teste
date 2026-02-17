9a2c1f4b1954ca377e37d31878ff9dca924d69e3
João Prado - E. Escolinha de TI - BNE <joaoprado@bne.com.br>
2026-02-17 17:48:31 -0300

    Adiciona C-Sharp como pasta normal

---
+++ C-Sharp/ESTAGIARIOS-PARTE-1-TESTE-DE-MESA-POR-ITERACAO/Problema-1/Problema-1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        int a, b, c;
+        a = 10;
+        b = 20;
+        c = ( a + b) / 2;
+        c = c - 40;
+        
+
+        int[] vetor = new int[4];
+        vetor [3] = a + b + c;
+
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.WriteLine($"|  Indice: {i} | Vetor: {vetor[i]}  |");
+    
+        }
+    }
+}
