615430bd17c8418d73713d338b6175cd82138871
João Prado - E. Escolinha de TI - BNE <joaoprado@bne.com.br>
2026-02-17 19:09:44 -0300

    Adiciona Problema-2 e Problema-3

---
+++ C-Sharp/ESTAGIARIOS-PARTE-1-TESTE-DE-MESA-POR-ITERACAO/Problema-2/Problema-2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        int a;
+
+        a = 2;
+        int[] vetor = new int[6];
+
+        while (a < 6)
+        {
+            vetor[a] = 10 * a;
+
+            a += 1;
+        }
+    }
+}
---
+++ C-Sharp/ESTAGIARIOS-PARTE-1-TESTE-DE-MESA-POR-ITERACAO/Problema-3/Problema-3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Programa
+{
+    static void Main()
+    {
+        int a, b;
+
+        a = 7;
+        b = a - 6;
+
+        int[] vetor = new int[6];
+
+        while (b < a)
+        {
+            vetor[b] = b + a; 
+            b = b + 2;
+
+
+        }
+    }
+}
