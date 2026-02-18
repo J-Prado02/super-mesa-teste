3e667f8f51ea4f5014869ec7c276dbee01443134
João Prado - E. Escolinha de TI - BNE <joaoprado@bne.com.br>
2026-02-18 11:18:20 -0300

    Adiciona o Problema-5

---
+++ C-Sharp/ESTAGIARIOS-TESTE-DE-MESA-POR-ITERACAO/Parte-2/Problema-5/Problema-5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class programa
+{
+    static void Main()
+    {
+        Console.Write("Saldo atual: ");
+        double saldoAtual = double.Parse(Console.ReadLine());
+
+        Console.Write("Taxa: ");
+        decimal percentual = decimal.Parse(Console.ReadLine());
+
+        Console.Write("Periodo: ");
+        int tempo = int.Parse(Console.ReadLine());
+
+        decimal taxa = percentual / 100;
+
+        double valorPresente = calculaJuros(saldoAtual, taxa, tempo);
+
+        Console.Write($"Valor Inicial Investido: {valorPresente:F2}");
+    }
+
+    static double calculaJuros (double valorFuturo, decimal taxaJuros, int periodo )
+    {
+        return valorFuturo / Math.Pow(1 + (double)taxaJuros, periodo);
+    }
+}
