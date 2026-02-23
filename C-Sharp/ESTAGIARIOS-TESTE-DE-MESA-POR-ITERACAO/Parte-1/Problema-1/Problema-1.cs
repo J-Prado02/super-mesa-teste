0f3564c226b01281848cb62322568238763c2be5
João Prado - E. Escolinha de TI - BNE <joaoprado@bne.com.br>
2026-02-23 12:59:55 -0300

    atualização

--- C-Sharp/ESTAGIARIOS-TESTE-DE-MESA-POR-ITERACAO/Parte-1/Problema-1/Problema-1.cs
+++ C-Sharp/ESTAGIARIOS-TESTE-DE-MESA-POR-ITERACAO/Parte-1/Problema-1/Problema-1.cs
@@ -13,7 +13,10 @@ class Programa
         int[] vetor = new int[4];
         vetor [3] = a + b + c;
 
-        for (int i = 0; i < vetor.Length; i++);
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.WriteLine(vetor[i]);
+        }
 
     }
 }
